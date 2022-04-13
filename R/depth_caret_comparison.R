79a4e865dd2ddeb8c23dad8e9f997eb4a7c44ca0
CamFreshwater <ccfreshwater@gmail.com>
2022-04-13 09:59:11 -0700

    update model comp script; bug with stage-specific models

--- R/depth_caret_comparison.R
+++ R/depth_caret_comparison.R
@@ -14,6 +14,18 @@ library(recipes)
 library(gbm)
 
 
+# parallelize based on operating system
+library("parallel")
+ncores <- detectCores() - 2
+if (Sys.info()['sysname'] == "Windows") {
+  library("doParallel")
+  cl <- makeCluster(ncores)
+  registerDoParallel(cl)
+} else {
+  doMC::registerDoMC(ncores)
+}
+
+
 # use 15 minute bins for fitting due to large number of observations
 depth_dat_raw <- readRDS(
   here::here("data", "depth_dat_15min.RDS")) %>% 
@@ -51,62 +63,32 @@ test_depth <- depth_dat %>% filter(ind_block == "5") %>% droplevels()
 
 ## MODELS ----------------------------------------------------------------------
 
-## define hyperparameters
-gbm_grid <-  expand.grid(interaction.depth = c(2, 5, 10), #c(3, 5, 9),
-                         n.trees = c(seq(10, 100, by = 10), 150, 200, 250, 300),
-                         shrinkage = 0.1,
-                         n.minobsinnode = c(5, 10, 20))
-
-# for RF two dimensions
-rf_grid <- expand.grid(tune_length = 8,
-                       n.trees = seq(100, 1000, by = 100))
-
-
-## define stage-specific datasets and consolidate all into tbls
-stage_foo <- function(dat, stage_in) {
-  dat %>% 
-    filter(stage == stage_in) %>% 
-    rename(stage_dat = stage)
-}
-
-train_mat <- stage_foo(train_depth, stage = "mature")
-test_mat <- stage_foo(test_depth, stage = "mature")
-train_imm <- stage_foo(train_depth, stage = "immature")
-test_imm <- stage_foo(test_depth, stage = "immature")
-
-
 ## create dataframe of models to compete
 model_tbl <- expand.grid(
   model_type = c("gbm", "rf"),
   response = c("logit_rel_depth", "rel_depth", "depth"),
   stage_dat = c("integrated", "mature", "immature")
   ) %>% 
-  as_tibble() %>% 
-  mutate(
-    hyper_pars = ifelse(model_type == "gbm", gbm_grid, rf_grid)
-  )
+  as_tibble() 
 
 # function to add data based on input variables
 add_data <- function(response, stage_dat) {
   # select dataset
   if (stage_dat == "integrated") {
     train_dum <- train_depth %>% 
-      dplyr::select(-ind_block)
+      dplyr::select(depth_var = response, latitude:stage, ind_block)
     test_dum <- test_depth %>% 
-      dplyr::select(-ind_block)
+      dplyr::select(depth_var = response, latitude:stage, ind_block)
   } 
   if (stage_dat %in% c("mature", "immature")) {
     train_dum <- stage_foo(train_depth, stage = stage_dat) %>% 
-      dplyr::select(-ind_block)
+      dplyr::select(depth_var = response, latitude:roms_temp, ind_block)
     test_dum <- stage_foo(test_depth, stage = stage_dat) %>% 
-      dplyr::select(-ind_block)
+      dplyr::select(depth_var = response, latitude:roms_temp, ind_block)
   }
   
   # subset based on response
-  list(
-    train = train_dum %>% select(depth_var = response, latitude:ind_block),
-    test = test_dum %>% select(depth_var = response, latitude:ind_block)
-  )
+  list(train = train_dum, test = test_dum)
 }
 
 dum <- map2(model_tbl$response %>% as.character(), 
@@ -115,22 +97,109 @@ dum <- map2(model_tbl$response %>% as.character(),
 model_tbl$train_data <- map(dum, function (x) x$train)
 model_tbl$test_data <- map(dum, function (x) x$test)
 
+
 ## add recipe 
-model_tbl$recipe <- map2(
+# NOTE ind_block needs to be retained to identify subsequent blocking but must
+# be removed from recipe and prior to fitting
+model_tbl$recipe <- purrr::map(
   model_tbl$train_data,
   function (x) {
-    dum_in <- x %>% dplyr::select(-ind_block)
+    dum <- x %>% 
+      dplyr::select(-ind_block)
     
-    recipe(depth_var ~ ., data = x) %>% 
+    recipe(depth_var ~ ., data = dum) %>% 
       step_impute_knn(all_predictors(), neighbors = 3) %>%
       step_nzv(all_predictors()) %>% 
       step_dummy(all_predictors(), -all_numeric())
 })
-depth_recipe <- recipe(logit_rel_depth ~ ., 
-                       data = train_depth %>% 
-                         dplyr::select(-ind_block)) %>% 
-  step_impute_knn(all_predictors(), neighbors = 3) %>%
-  step_nzv(all_predictors()) %>% 
-  step_dummy(all_predictors(), -all_numeric())
 
 
+## shared settings
+train_folds <- groupKFold(model_tbl$train_data[[1]]$ind_block,
+                          k = length(unique(model_tbl$train_data[[1]]$ind_block)))
+ctrl <-   trainControl(
+  method="repeatedcv",
+  index = train_folds
+)
+
+## define hyperparameters
+gbm_grid <-  expand.grid(interaction.depth = c(2, 5, 10),
+                         n.trees = c(seq(10, 100, by = 10), 150, 200, 250, 300),
+                         shrinkage = 0.1,
+                         n.minobsinnode = c(5, 10, 20))
+
+rf_grid <- expand.grid(tune_length = 8,
+                       n.trees = seq(100, 500, by = 100))
+
+
+model = sub_tbl$model_type[[1]]
+recipe = sub_tbl$recipe[[1]]
+train_data = sub_tbl$train_data[[1]]
+
+fit_foo <- function(model, recipe, train_data) {
+  if (model == "gbm") {
+    fit <- train(
+      recipe,
+      train_data %>% dplyr::select(-ind_block),
+      method = "gbm", 
+      metric = "RMSE",
+      maximize = FALSE,
+      trControl = ctrl,
+      tuneGrid = gbm_grid[1:2, ]
+    )
+  }
+  if (model == "rf") {
+    for (i in 1:2) {
+      fit <- train(
+        recipe,
+        train_data %>% dplyr::select(-ind_block),
+        method = "ranger", 
+        metric = "RMSE",
+        maximize = FALSE,
+        tuneLength = 2,#unique(rf_grid$tune_length),
+        trControl = ctrl,
+        num.trees = rf_grid$n.trees[i]
+      ) 
+    }
+  }
+  list(results = fit$results,
+       top_model = fit$finalModel)
+}
+
+
+tt <- fit_foo(sub_tbl$model_type[[1]],
+        sub_tbl$recipe[[1]],
+        sub_tbl$train_data[[1]])
+
+
+sub_tbl <- model_tbl[13:14,]
+
+fit_list <- pmap(list(sub_tbl$model_type,
+                      sub_tbl$recipe,
+                      sub_tbl$train_data),
+                 .f = fit_foo)
+
+depth_gbm <- train(
+  model_tbl$recipe[[13]],
+  model_tbl$train_data[[13]] %>% dplyr::select(-ind_block),
+  method = "gbm", 
+  metric = "RMSE",
+  maximize = FALSE,
+  # tuneLength = 2,
+  trControl = ctrl,
+  tuneGrid = gbm_grid[1:2, ]
+)
+
+depth_rf <- train(
+  model_tbl$recipe[[1]],
+  model_tbl$train_data[[1]] %>% dplyr::select(-ind_block),
+  method = "ranger", 
+  metric = "RMSE",
+  maximize = FALSE,
+  tuneLength = 3,
+  trControl = ctrl,
+  num.trees = 100
+)
+
+
+min(depth_gbm$results$RMSE)
